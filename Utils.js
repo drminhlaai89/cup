2c76633708cd7dc9f26b0f9ae123a4d67b8a217a
Nhan <drminhlaai78@gmail.com>
2024-01-10 11:45:49 +0700

    Update comment

--- Utils.js
+++ Utils.js
@@ -26,6 +26,7 @@ class Reticle extends THREE.Object3D {
 // const modelURL = './static/sunflower.gltf';
 window.gltfLoader.load("https://drminhlaai89.github.io/cup/static/s.glb", function(gltf) {
   const flower = gltf.scene.children.find(c => c.name === 's.glb')
+  //Comment on this thing.
   //flower.castShadow = true;
   window.sunflower = gltf.scene;
   console.log("Already Instantiate");
